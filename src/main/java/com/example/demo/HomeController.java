8279e0f2ba0418be3c96470261be2ad457473ac6
GBTC440018UR <GBTC440018UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-11-14 15:13:16 -0500

    working

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,22 +2,28 @@ package com.example.demo;
 
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.io.IOException;
+import java.security.Principal;
 import java.util.Map;
 
 @Controller
 public class HomeController {
+
 
     @Autowired
     ActorRepository actorRepository;
 
     @Autowired
     CloudinaryConfig cloudc;
+
 
     @RequestMapping("/")
     public String listActors(Model model) {
